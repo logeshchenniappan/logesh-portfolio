a6193177a0dbadf1083d5f15f89f0c8ddb061ada
Logesh C <91900910+logeshchenniappan@users.noreply.github.com>
2025-05-29 16:00:57 +0530

    Update portfolio.config.ts

--- src/config/portfolio.config.ts
+++ src/config/portfolio.config.ts
@@ -6,7 +6,7 @@ export const portfolioConfig: Portfolio = {
   resume: "https://drive.google.com/file/d/1maKM3isLbHN7IKbHKGorxXkkoFf5Sqfv/view?usp=sharing",
   links: {
     github: "https://github.com/logeshchenniappan",
-    twitter: "https://x.com/c__logesh",
+    twitter: "https://x.com/logesh__c",
     mail: "mailto:logeshc17@gmail.com",
     discord: "https://discordapp.com/users/841126921886498817",
     linkedin: "https://linkedin.com/in/logeshchenniappan",
